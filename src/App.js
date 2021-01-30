228a8a4584b6cb63e66810450947377d358ba293
Simon <simonyacoub@cmail.carleton.ca>
2021-01-30 14:31:29 -0500

    Buggy Code 1
    error in dashboard???

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
-import Dashboard from './pages/Dashboard';
+import Home from './pages/Home' 
+import Dashboard from './pages/Dashboard'
+
 
 function App() {
   return (
     <div>
       <h1>COVID BBY</h1>
-      <button> Hello </button>
-      <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+      <Home />
+      <Dashboard />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+
+
+function Home(){
+  return(
+  <div>
+    
+    <button> Hello </button>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </div>
+
+  ); //return
+    
+}
+export default Home;
