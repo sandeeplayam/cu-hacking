e4a749fad2aae77b20b05a127f21053787fd29c7
sandeeplayam <sandeeplayam@cmail.carleton.ca>
2021-01-30 16:04:36 -0500

    Added routing to react application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Home />
-        <Dashboard />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route
+            path="/"
+            render={() => (
+              <div>
+                <h1>404 Error</h1>
+              </div>
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
