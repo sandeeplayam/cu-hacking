873c715ccd414b916bd3f30c265b73e0c5a7b793
Simon <simonyacoub@cmail.carleton.ca>
2021-01-30 14:12:04 -0500

    Testing Material UI with buttons
    Creating a button using material ui  variation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div>
       <h1>COVID BBY</h1>
-      <Dashboard />
+      <button> Hello </button>
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
     </div>
   );
 }
 
 export default App;
+
