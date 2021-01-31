3ca870626f9b86461d66a1e2de668946caf26e6d
Simon <simonyacoub@cmail.carleton.ca>
2021-01-31 00:46:19 -0500

    Working Stats

--- src/components/Home/Stats/Stats.jsx
+++ src/components/Home/Stats/Stats.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styles from './Stats.module.css';
 
 function Stats() {
+    const [statInfo, setStatInfo] = useState([]);
+    
+    useEffect(() => {
+         fetch('https://disease.sh/v3/covid-19/all')
+            .then((response) => response.json())
+            .then((data) => {
+              setStatInfo(data);
+            });
+    }, []);
+
+
     return (
         <div className={styles.main}>
-            <div className={styles.caseStatBar}>Total Cases:</div>
-            <div className={styles.deathStatBar}>Total Deaths: </div>
+            <div className={styles.caseStatBar}> <p>Total Cases: {statInfo.cases}</p></div>
+            <div className={styles.deathStatBar}><p>Total Deaths: {statInfo.deaths}</p> </div>
         </div>
     );
 }
